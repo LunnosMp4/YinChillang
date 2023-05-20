d907251d8c0bced6b579eb700f257aebe080ef34
LunnosMp4 <loic.tisseyre@epitech.eu>
2023-05-20 15:48:56 +0200

    Add include

---
+++ include/YinChillang.hpp
@@ -0,0 +1,13 @@
+//
+// Created by oyco on 20/05/23.
+//
+
+#pragma once
+
+#include "raylib.h"
+#include "raymath.h"
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
